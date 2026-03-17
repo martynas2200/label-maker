b6bc872773030d515f8753aa041288d02eed7d30
Martynas <martynas2200@gmail.com>
2026-03-17 10:51:34 +0100

    fix: tiptap dependency

--- vite.config.js
+++ vite.config.js
@@ -55,7 +55,17 @@ export default defineConfig(({ command }) => ({
 		}),
 		setupWwwPlugin(),
 	],
-	resolve: {},
+	resolve: {
+		alias: [
+			...["@tiptap/core", "@tiptap/vue-3"].reduce((acc, pkg) => {
+				const entry = path.resolve(__dirname, `node_modules/${pkg}/dist/index.js`);
+				if (fs.existsSync(entry)) {
+					acc.push({ find: pkg, replacement: entry });
+				}
+				return acc;
+			}, []),
+		],
+	},
 	base: command === "build" ? "/assets/label_maker/labels_assets/" : "/",
 	css: {
 		postcss: "./postcss.config.js",
