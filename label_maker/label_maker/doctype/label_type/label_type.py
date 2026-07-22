2b1739b501e671305a1388cd0adf337caed17742
Martynas <martynas2200@gmail.com>
2026-07-22 20:55:52 +0300

    Prep for custom label formats

--- label_maker/label_maker/doctype/label_type/label_type.py
+++ label_maker/label_maker/doctype/label_type/label_type.py
@@ -1,11 +1,30 @@
 # Copyright (c) 2024, Label Maker Contributors
 # See license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
-
-# from frappe import _
+from frappe.utils.jinja import validate_template
 
 
 class LabelType(Document):
-	pass
+	def validate(self):
+		"""Validate Jinja2 templates so the user gets immediate feedback."""
+		if self.print_format == "html" and self.template:
+			self._validate_jinja(self.template, "HTML Body Template")
+		if self.print_format == "html" and self.css:
+			# we must also check for Jinja2 expressions in CSS
+			if "{{" in self.css or "{%" in self.css:
+				self._validate_jinja(self.css, "Custom CSS")
+		if self.print_format == "zpl" and self.zpl_template:
+			self._validate_jinja(self.zpl_template, "ZPL Template")
+
+	def _validate_jinja(self, source: str, field_label: str) -> None:
+		"""Parse the Jinja2 template and raise a ValidationError on syntax errors."""
+		try:
+			validate_template(source)
+		except Exception as e:
+			frappe.throw(
+				_("Syntax error in {0}: {1}").format(field_label, str(e)),
+				frappe.ValidationError,
+			)
